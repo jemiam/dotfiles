eb286ded6680ddcd678c7e8328cc623d4db6f4c1
Manabu Ejima <jemiam@gmail.com>
2013-12-09 15:22:58 +0900

    Set zsh to default shell

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ find . -mindepth 1 -maxdepth 1 -name ".*" | while read line; do
   ln -sfn $HOME/dotfiles/$line $HOME/$line
 done
 
+chsh -s /bin/zsh
+
+# for system wide install
+# cp rbenv.sh /etc/profile.d/
 ~/.vim/bundle/neobundle.vim/bin/neoinstall
