d5ae6663cf760a78e3447aab1bb802f24ee714e0
Manabu Ejima <jemiam@gmail.com>
2014-02-26 16:36:23 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 find . -name ".*" -mindepth 1 -maxdepth 1 | while read line; do
   ln -sfn $HOME/dotfiles/$line $HOME/$line
 done
+
+git submodule init
+git submodule update
+#git submodule foreach 'git checkout master; git pull'
